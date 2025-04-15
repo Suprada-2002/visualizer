d5970da9b54ab21bc4d4c8833261ad4d7c98eb07
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-15 18:34:22 +0530

    Create InputBar.jsx

---
+++ src/pages/sortingComponents/InputBar.jsx
@@ -0,0 +1,50 @@
+function InputBar({length, speed, isSorting, generateRandomArray, handleSpeed, handleLength, 
+    algo, setAlgo, handleSort}) {
+
+    return(
+        <>
+        <p>Inputs: </p>
+
+        <form>
+                <label>Speed</label>
+                <input 
+                type="range"
+                min="1"
+                max="10"
+                value={Math.ceil(400/ speed)}
+                disabled={isSorting}
+                onChange={handleSpeed}
+                ></input>
+
+                <label>Length: </label>
+                <input type="range" 
+                min='5'
+                max={100}
+                step='1'
+                value={length}
+                disabled={isSorting}
+                onChange={handleLength}
+                 />
+                {/* <label>Speed: </label>
+                <input type="number" min={30} max={90}/> */}
+                <label>Select Algorithm:  </label>
+                <select 
+                name="algo"
+                value={algo}
+                disabled={isSorting}
+                onChange={(e) => setAlgo(e.target.value)}
+                >
+                    <option value="bubbleSort">Bubble Sort</option>
+                    <option value="selectionSort">Selection Sort</option>
+                    <option value="insertionSort">Insertion Sort</option>
+                </select>
+
+                <button onClick={generateRandomArray} disabled={isSorting}>Randomize</button>
+                <button onClick={handleSort} disabled={isSorting}>Sort</button>
+            </form>
+
+        </>
+    )
+}
+
+export default InputBar;
