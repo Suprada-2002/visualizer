4d6c347a775b65caba938f29db527b044f27dd26
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-16 19:38:29 +0530

    Update Visualizer.jsx

--- src/pages/searchComponents/Visualizer.jsx
+++ src/pages/searchComponents/Visualizer.jsx
@@ -6,21 +6,19 @@ import { linearSearch, binarySearch, exponentialSearch} from "./Algorithm";
 
 function Visualizer(){
 
-        const [length, setLength] = useState(40);
+        const [length, setLength] = useState(6);
         const [searchingEle, setIsSearchingEle] = useState(Math.floor(Math.random() * (length - 1 + 1) + 1));
         const [blocks, setBlocks] = useState([]);
         const [isSearching, setIsSearching] = useState(false);
-        const [algo, setAlgo] = useState("bubbleSort");
+        const [algo, setAlgo] = useState("linearSearch");
         const [speed, setSpeed] = useState(250);
-        const [checked, setChecked] = useState([]);
+        const [checked, setChecked] = useState([]); // left & right
+        const [mid, setMid] = useState([]); //mid
         const [found, setFound] = useState([]);
-        const [index, setIndex] = useState(0);
 
         useEffect(() => {
             generateRandomArray(length);
         }, [algo, length])
-
-        // useEffect(() => console.log(searchingEle), [searchingEle])
 
         function generateRandomArray(length){
             setIsSearching(false);
@@ -34,7 +32,7 @@ function Visualizer(){
             }
             setBlocks(randomArray);
             setIsSearchingEle(randomArray[Math.floor(Math.random() * length)]);
-        }
+        };
 
         function handleSpeed(event) {
             setSpeed(400/Number(event.target.value))
@@ -44,84 +42,54 @@ function Visualizer(){
             setLength(Number(event.target.value))
         }
 
-        // function handleSearch(){
-        //     setIsSearching(true);
-        //     switch(algo){
-        //         case "linearSearch":
-        //             searchBlocks(linearSearch(blocks, searchingEle));
-        //             setIsSearching(true);
-        //             break;
-        //         case "binarySearch":
-        //             searchBlocks(binarySearch(blocks, searchingEle));
-        //             setIsSearching(true);
-        //             break;
-        //         default:
-        //             setIsSearching(false);
-        //             console.log("Invalid Algorithm!!");
-        //     }
+        function handleSearch(event){
+            event. preventDefault()
+            setIsSearching(true);
+            setChecked([]);
+            setFound([]);
+            setMid([]);
 
-        //     const searchBlocks = (order) =>{
-        //         let i = 0;
-        //         const process = () => {
-        //             if(i>= order.length){
-        //                 setIsSearching(false);
-        //                 return;
-        //             }
+            const searchBlocks = (order) =>{
+                console.log("order", order)
+                let i = 0;
 
-        //             const [checked, found, mess] = order[i];
-        //             setChecked([checked]);
-        //             setFound([found]);
+                const processOrder = () =>{
+                    if(i>=order.length){
+                        setIsSearching(false);
+                        return;
+                    }
 
-        //             i++;
-        //             setTimeout(process, speed);
-        //         };
-        //         process();
-        //     }
-           
-        
-          
-            // setIsSearching(true);
-            // algo === "linearSearch" ? searchBlocks(linearSearch(blocks)):
-            // algo === "binarySearch" ? searchBlocks(binarySearch(blocks)):
-            // algo === "exponentialSearch" ? searchBlocks(exponentialSearch(blocks)) : (() => {
-            //     setIsSearching(false);
-            // })()
-        // }
-        const handleSearch = (order) => {
-            let i = 0;
-        
-            const processOrder = () => {
-                if (i >= order.length) {
-                    setIsSearching(false);
-                    return;
-                }
-        
-                const [left, mid, right, foundIndex] = order[i];
-                setChecked([left, mid, right]);
-        
-                if (foundIndex !== null) {
-                    setFound((prevState) => [...prevState, foundIndex]);
-                }
-        
-                i++;
-                setTimeout(processOrder, speed);
+                    const [left, right, mid , found, arr] = order[i];
+                    if(arr) setBlocks(arr);
+                    setChecked([left, right]);
+                    setMid([mid]);
+
+                    if(found !== null) setFound([found]) 
+                    // else{ Window.alert("Element not found in the array!!")}
+
+                    i++;
+                    setTimeout(processOrder, speed);
+                };
+
+                processOrder(); 
             };
-        
-            processOrder();
+            
+            setIsSearching(true);
+            algo === "linearSearch" ? searchBlocks(linearSearch(blocks, searchingEle)) :
+            algo === "binarySearch" ? searchBlocks(binarySearch(blocks, searchingEle)) : (() => { setIsSearching(false) })()
         };
-        
-
 
     return(
         <>
         <h3>Sorting Algorithm</h3>
 
         <InputBar length={length} speed={speed} isSorting={isSearching} 
-        generateRandomArray={generateRandomArray} handleSpeed={handleSpeed} searchingEle={searchingEle}
-        handleLength={handleLength} algo={algo} setAlgo={setAlgo} handleSort={handleSearch}
+        generateRandomArray={generateRandomArray} handleSpeed={handleSpeed}
+        handleLength={handleLength} algo={algo} setAlgo={setAlgo} handleSearch={handleSearch}
         />
+        <span>Element to be Searched: {searchingEle}</span>
 
-        <Bars blocks={blocks} checked={checked} found={found} sortedIndex={index}/>
+        <Bars blocks={blocks} checked={checked} mid={mid} found={found}/>
         </>
     )
 }
