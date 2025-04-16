b73b20963a94deb060d2515f961ec84f0f1255d8
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-16 19:31:09 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
+import './Home.css';
+import { useNavigate } from 'react-router-dom';
+
 function Home(){
+
+    const navigate = useNavigate();
+
     return(
         <>
         <p>Home</p>
+        <div className="algoBoxes">
+
+            <div className="box" onClick={() => navigate('/sorting')}>
+                <h4>Sorting Algorithms</h4>
+                <p>Algorithms Implemented: </p>
+                <ul>
+                    <li>Bubble Sort</li>
+                    <li>Selection Sort</li>
+                    <li>Insertion Sort</li>
+                    <li>Merge Sort</li>
+                    <li>Quick Sort</li>
+                </ul>
+            </div>
+
+            <div className="box" onClick={() => navigate('/searching')}>
+                <h4>Searching Algorithms</h4>
+                <p>Algorithms Implemented: </p>
+                <ul>
+                    <li>Linear Search</li>
+                    <li>Binary Search</li>
+                    <li>Exponential Search</li>
+                </ul>
+            </div>
+
+        </div>
         </>
     )
 }
