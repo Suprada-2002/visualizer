235586fa7b67b7cf85b53de69863a1bdb005653f
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-28 19:46:18 +0530

    Update Bars.jsx

--- src/pages/sortingComponents/Bars.jsx
+++ src/pages/sortingComponents/Bars.jsx
@@ -1,51 +1,50 @@
-import { useEffect, useState } from "react";
-import './Bars.css'
+import React, { useState, useEffect } from 'react'
+import './Bars.css';
 
-function Bars({blocks, swap, compare, sortedIndex}){
-  
-    const [width, setWidth] = useState(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 5));
+function Bars({ blocks, compare, sorted, swap }){
+    const [width, setWidth] = useState(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 5))
+    const color = blocks.length <= 50 && width > 14 ? '#e1e4e8' : 'transparent'
 
     useEffect(() => {
-      const handleResize = () => {
+        const handleResize = () => {
+            setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8))
+        }
+
+        window.addEventListener('resize', handleResize)
+
         setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8))
-    }
-    window.addEventListener('resize', handleResize);
-    setWidth(Math.min(20, Math.ceil(window.innerWidth / blocks.length) - 8));
     }, [blocks.length])
 
-    return(
-        <>
-          <div className="bars">
-            {blocks.map( (num, key) => {
+    return (
+        <div className='barBlocks'>
 
-                const height = num * 500 / blocks.length;
-                
-                let bg = 'blue';
+            {blocks.map((block, i) => {
+                const height = block * 500 / blocks.length;
+                let bg = '#414853'
 
-                if(compare && (key === compare[0] || key === compare[1])){
-                  bg = 'yellow';
+                // i th element is being compared with some other element
+                if(compare && (i === compare[0] || i === compare[1])){
+                    bg = '#2E88C7'
                 }
 
-                if(swap && (key === swap[0] || key === swap[1])){
-                  bg = 'pink';
+                if(swap && (i === swap[0] || i === swap[1])){
+                    bg='#F81774'
+                }
+                // i th element is in its correct position
+                if(sorted && sorted.includes(i)){
+                    bg = '#298459'
                 }
 
-                if(sortedIndex && sortedIndex.includes(key)){
-                  bg = 'green';
-                }
-                
-                const barStyle = {
+                const style = {
                     'backgroundColor': bg,
-                    'color': 'black', 
+                    'color': color, 
                     'height': height, 
                     'width': width,
-                    'border':'1px solid black'
                 }
-                return <div key={key} className="bar" style={barStyle}>{num}</div>
+                return (<div key={i} className='block' style={style}>{block}</div>)
             })}
-          </div>
-        </>
-    )
+        </div>
+    );
 }
 
 export default Bars;
