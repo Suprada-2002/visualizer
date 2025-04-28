22ce49cd908294467287ec5ab38d1f14819de598
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-04-28 20:59:13 +0530

    Update Legend.jsx

--- src/pages/sortingComponents/Legend.jsx
+++ src/pages/sortingComponents/Legend.jsx
@@ -1,11 +1,23 @@
-function Legend() {
-  return(
-    <>
-      <div className="legend"> 
-        <p>legend</p>
-      </div
-    </>
-  )
+import {SwatchBook, CheckCheck, GitCompare, Ruler} from 'lucide-react'
+const Legend = ({algo, len}) => {
+
+    return (
+        <div className='d-flex justify-content-center pt-2'>
+             <div className='align-items-center'> 
+             <span className='sq compare'></span> <Ruler color='#da8b17' /> Size: {len}
+            </div>
+            <div className='align-items-center'> 
+                <span className='sq compare'></span> <GitCompare color='#2E88C7' /> Compare
+            </div>
+            <div className='align-items-center'> 
+                <span className='sq swap'></span> <SwatchBook color='#F81774' /> {algo !== 'mergeSort' ? 'Swap' : 'Taking From Auxillary Space'}
+            </div>
+            <div className='align-items-center'> 
+                <span className='sq sorted'></span> <CheckCheck color="#4bc52e" /> Sorted
+            </div>
+
+        </div>
+    )
 }
 
 export default Legend;
